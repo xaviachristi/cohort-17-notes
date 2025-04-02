bb7b1fdfda058adc9d129d316ad6bf6e8d089cbd
Dan <peritract@hotmail.co.uk>
2025-04-02 09:46:40 +0100

    Clean up

--- software-fundamentals/backends/main.py
+++ software-fundamentals/backends/main.py
@@ -20,69 +20,51 @@ def company_index():
     if request.method == "GET":
 
         if "country" in parameters:
-            # return just those matching the country
-            # matched_companies = []
-            # for company in companies:
-            #     if company["country"] == parameters["country"]:
-            #         matched_companies.append(company)
-            # return matched_companies
-            # return [c for c in companies if c["country"] == parameters["country"]]
-            return list(filter(lambda x: x["country"] == parameters["country"], companies))
+            return [c for c in companies if c["country"] == parameters["country"]]
         else:
             return companies
 
     if request.method == "POST":
-        new_company = json.loads(request.data) # request.json
+        new_company = request.json
         if not is_valid_company(new_company):
-            return {"Error": True, "message": "Not a valid company"}, 400
-        company_id = max(company.get("id") for company in companies) + 1
-        company_to_add = {"id": company_id}
-        company_to_add["name"] = new_company.get("name")
-        company_to_add["country"] = new_company.get("country")
-        company_to_add["domain"] = new_company.get("domain")
-        company_to_add["stock"] = new_company.get("stock")
-        company_to_add["email"] = new_company.get("email")
-        companies.append(company_to_add)
-        return company_to_add, 201
+            return {"error": True, "message": "Not a valid company"}, 400
+        
+        company_id = get_next_id(companies)
+
+        new_company["id"] = company_id
+
+        companies.append(new_company)
+
+        return new_company, 201
 
 
 def is_valid_company(company: dict) -> bool:
+    """Returns True if a dict has all required keys."""
     return all(key.lower() in company for key in ["name", "country"])
 
 
-@api.route("/company/<int:id>", methods=["GET"])
+def get_next_id(companies: list[dict]) -> int:
+    """Returns the next valid company ID."""
+    return max(company.get("id") for company in companies) + 1
+
+
+@api.route("/company/<int:id>", methods=["GET", "DELETE"])
 def company_show(id: int):
-    """Returns data on a single company."""
-    for c in companies:
-        if c["id"] == id:
-            return c
-    # return abort(404)  # Standard error message
-    return f"Unable to find a company with ID {id}", 404 # Custom
+    """Returns data on a single company or deletes a company."""
+    if request.method == "GET":
+        for c in companies:
+            if c["id"] == id:
+                return c
+        return {"error": True, "message": f"Unable to find a company with ID {id}"}, 404
+    else:
+        companies = [c for c in companies if c["id"] != id]
+        return { "success": True }
 
 
 @api.get("/company/random")
 def company_random():
     """Returns a random company's details."""
-    return choice(companies)   
-
-"""
-/ceo (GET, POST)
-/ceo/id (GET, PATCH, DELETE)
-
-"""
-
-
-"""
-Write an endpoint that will let the user delete a company. 
-The data is of type list.
-
-For example, the query:
-
-http://127.0.0.1:5000/companies/212
-
-should delete the company with the id equal to 212
-"""
-...
+    return choice(companies)
 
 
 if __name__ == "__main__":
