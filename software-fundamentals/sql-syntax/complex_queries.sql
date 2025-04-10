6e605bd34c0cf7ed48fd892758d962aebfc71b11
ruyzambrano <ruyzambrano@gmail.com>
2025-04-10 11:00:35 +0100

    Add solutions

--- software-fundamentals/sql-syntax/complex_queries.sql
+++ software-fundamentals/sql-syntax/complex_queries.sql
@@ -45,7 +45,54 @@ GROUP BY p.pokemon_name, p.pokemon_weight
 LIMIT 10;
 
 -- Write a query that finds the name and types of each pokemon that has more than one type
+WITH 
+    type_count AS (
+        SELECT ta.pokemon_id, COUNT(ta.pokemon_id) AS count FROM pokemon
+        JOIN type_assignment as ta USING(pokemon_id)
+        JOIN pokemon_type as t USING(type_id)
+        GROUP BY ta.pokemon_id
+    )
 
+SELECT p.pokemon_name, STRING_AGG(t.type_name, ', ') AS types
+FROM pokemon as p
+JOIN type_assignment as ta USING(pokemon_id)
+JOIN pokemon_type as t USING(type_id)
+JOIN type_count USING (pokemon_id)
+WHERE type_count.count > 1
+GROUP BY p.pokemon_name
+;
+
+
+WITH 
+    multitypes AS (
+        SELECT pokemon_id
+        FROM type_assignment
+        GROUP BY pokemon_id
+        HAVING COUNT(*) > 1
+    )
+SELECT p.pokemon_name, STRING_AGG(t.type_name, ', ') AS types
+FROM pokemon as p
+JOIN type_assignment as ta USING(pokemon_id)
+JOIN pokemon_type as t USING(type_id)
+WHERE p.pokemon_id IN (
+    select pokemon_id FROM multitypes
+)
+GROUP BY p.pokemon_id, p.pokemon_name
+LIMIT 10;
+
+SELECT * FROM pokemon;
+
+SELECT pokemon_name, pokemon_id FROM pokemon;
+
+SELECT pokemon_name, pokemon_id 
+FROM pokemon
+WHERE pokemon_name ILIKE '%SAUR%';
+
+
+SELECT pokemon_name, pokemon_id, p.pokemon_weight
+FROM pokemon
+WHERE pokemon_name ILIKE '%SAUR%'
+GROUP BY pokemon_id;
 
 -- The average height of fire-type Pokemon compared to water-type Pokemon
 
